9182ba1945961d351a7c2084f6deceee7d3dc790
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-10-23 12:40:47 +0300

    Error Message Set and Read

--- cppsrc/utility.cpp
+++ cppsrc/utility.cpp
@@ -1,6 +1,19 @@
 #include "utility.h"
 
+#include <QDebug>
+
 Utility::Utility(QObject *parent) : QObject(parent)
 {
 
+}
+
+QString Utility::errorMessage() const
+{
+    return mErrorMessage;
+}
+
+void Utility::setErrorMessage(const QString &errorMessage)
+{
+    mErrorMessage = errorMessage;
+    emit errorMessageChanged(mErrorMessage);
 }
--- cppsrc/utility.h
+++ cppsrc/utility.h
@@ -4,15 +4,28 @@
 #include <QtCore/QObject>
 #include <QtCore/qglobal.h>
 
+
 class Utility : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString errorMessage READ errorMessage WRITE setErrorMessage NOTIFY errorMessageChanged)
 public:
     explicit Utility(QObject *parent = nullptr);
 
+    QString errorMessage() const;
+    void setErrorMessage(const QString &errorMessage);
+
 signals:
+    void errorMessageChanged(QString mesaj);
+
 
 public slots:
+
+
+private:
+
+
+    QString mErrorMessage;
 };
 
 #endif // UTILITY_H
