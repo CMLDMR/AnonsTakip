a1623201ffd12fa29666f1b518eea94cca737dbf
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-10-23 11:06:44 +0300

    Koleksiyon Eklendi.

--- cppsrc/dbkey.cpp
+++ cppsrc/dbkey.cpp
@@ -4,3 +4,13 @@ DBKey::DBKey(QObject *parent) : QObject(parent)
 {
 
 }
+
+QString DBKey::collection() const
+{
+    return mCollection;
+}
+
+void DBKey::setCollection(const QString &collection)
+{
+    mCollection = collection;
+}
--- cppsrc/dbkey.h
+++ cppsrc/dbkey.h
@@ -7,12 +7,23 @@
 class DBKey : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString collection READ collection )
 public:
     explicit DBKey(QObject *parent = nullptr);
+
+
+
+
+    QString collection() const;
+    void setCollection(const QString &collection);
 
 signals:
 
 public slots:
+
+
+private:
+    QString mCollection = "AnonsCihazları";
 };
 
 #endif // DBKEY_H
